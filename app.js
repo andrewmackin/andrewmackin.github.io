51efd32e048c89ea3b51d10963579157682b02c6
Andrew Mackin <andrwmack@gmail.com>
2023-01-16 22:40:27 -0500

    Remove console log

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 /* Section Slide Animations */
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry);
     entry.target.classList.toggle('show', entry.isIntersecting)
   });
 });
